5985e0e13d56b18f29261b323071ba61d19a8839
palSachin2003 <palshachin8090@gmail.com>
2025-04-10 15:54:18 +0530

    update

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <Navbar />
-      <div className="container mt-28 mx-auto px-12 py-4">
+      <div className="container mt-28 mx-auto md:px-12 px-6 py-4">
         <HeroSection />
         {/* <AchievementsSection /> */}
         <AboutSection />
